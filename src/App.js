fc39748c69d1c19063eef43cda0ed4af20451e25
star0717 <ydb0717@gmail.com>
2022-12-13 21:58:47 +0900

    a

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import ContextEx from "./ContextEx";
 import EffectEx from "./EffectEx";
 import ReducerEx from "./ReducerEx";
 import StateEx from "./StateEx";
+import MemoEx from "./MemoEx";
 
 const App = () => {
   return (
     // 무조건 빈태그로 감싸야됨
     <>
-      <StateEx />
+      {/* <StateEx />
       <EffectEx />
       <ContextEx />
-      <ReducerEx />
+      <ReducerEx /> */}
+      <MemoEx />
     </>
   );
 };
--- src/MemoEx.js
+++ src/MemoEx.js
@@ -1,15 +1,56 @@
-import React,{useMemo, useState} from "react";
-const getAverage = numbers => {
+import React, { useState, useMemo } from "react";
+
+const getAverage = (numbers) => {
   console.log("평균값 계산중");
-}
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+};
 
 const MemoEx = () => {
   const [list, setList] = useState([]);
-  const [number, setNumber] = useState('');
+
+  const [number, setNumber] = useState("");
+
+  const onChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const onInsert = (e) => {
+    const nextList = list.concat(parseInt(number));
+    setList(nextList);
+    setNumber("");
+  };
+  // concat = 두개의 문자열을 하나의 문자열로 만들어주는 함수
+  // var a = "안녕"
+  // var b = "하세요"
+  // console.log((a.concat(b)))
+  // = 안녕하세요
+
+  // parseInt = 정수화
+
+  const avg = useMemo(() => getAverage(list, [list]));
   return (
     // 무조건 빈태그로 감싸야됨
     <>
-      <input value={number}/>
+      <input
+        value={number}
+        onChange={onChange}
+        type="number"
+        placeholder="Only number"
+      />
+      <button onClick={onInsert}>등록</button>
+
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <div>
+        <h2>
+          평균 값 : <p>{avg}</p>
+        </h2>
+      </div>
     </>
   );
 };
